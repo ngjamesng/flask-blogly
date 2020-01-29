6f3940fd0dd2f4aa3c109758f680076465f6e2c9
ngjamesng <just.james.ng@gmail.com>
2020-01-29 12:05:42 -0800

    add some routes, class for user, and base.html

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+app.config["SECRET_KEY"] = "keyyyy"
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """connects to database!"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """user model"""
+
+    __tablename__ = "users"
+
+    def __repr__(self):
+        """Show user info"""
+
+        return f"<full name: {self.first_name} {self.last_name}>"
+
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(
+        db.String(),
+        nullable=False,
+        default="https://image.shutterstock.com/image-vector/profile-blank-icon-empty-photo-260nw-535853269.jpg",
+    )
+
